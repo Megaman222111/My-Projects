7e00c7df0cbe4971fc11c5c81cbba3ce63acee8a
Megaman222111 <74266911+Megaman222111@users.noreply.github.com>
2020-11-25 18:53:07 -0700

    Add files via upload
    Here, I took 2 numbers from the user, and switched them so the first number would be the 2nd, and the 2nd number would be the first.

---
+++ Switching 2 Numbers and Displaying them.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2;
+        cout<<"Give a Number: ";
+       cin>>num1;
+    
+        cout<<"Give Another Number: ";
+        cin>>num2;
+    cout<<"The numbers switched are: "<<num2<<" "<<num1;
+
+}
