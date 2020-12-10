c6fd724f1af19f1034a07527a81933d8fb208db5
Megaman222111 <74266911+Megaman222111@users.noreply.github.com>
2020-12-09 18:49:42 -0700

    Add files via upload

---
+++ My 2nd Class in C++.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+class Student {
+
+public:
+
+int id;
+
+string name;
+
+void insert(int i, string n)
+
+{
+
+id = i;
+
+name = n;
+
+}
+
+void display()
+
+{
+
+cout<<id<<" "<<name<<endl;
+
+}
+
+};
+
+int main(void) {
+
+Student s1;
+
+Student s2;
+
+s1.insert(201, "Sonoo");
+
+s2.insert(202, "Nakul");
+
+s1.display();
+
+s2.display();
+
+return 0;
+
+}
