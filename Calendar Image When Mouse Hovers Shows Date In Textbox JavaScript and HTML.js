e0fb94340fd61f2ad5cfea3d04a1341c3acc8f89
Megaman222111 <74266911+Megaman222111@users.noreply.github.com>
2021-04-07 22:17:06 -0600

    Add files via upload

---
+++ Calendar Image When Mouse Hovers Shows Date In Textbox JavaScript and HTML.js
@@ -0,0 +1,9 @@
+function Hovering_Cursor_On_Calendar(){
+    var d= new Date();
+    document.getElementById("t1").value=d;
+    document.getElementById("t1").value=d;
+}
+
+function Not_Hovering_Cursor_On_Calendar(){
+    document.getElementById("t1").value="";
+}
---
+++ Hover Over Image And Image Changes HTML and JavaScript.js
@@ -0,0 +1,7 @@
+function testNew(){
+    document.getElementById("img1").src="Hand for Hover Over Image And Image Changes HTML and JavaScript.png";
+}
+
+function testOld(){
+    document.getElementById("img1").src="Cursor for Hover Over Image And Image Changes HTML and JavaScript.png";
+}
