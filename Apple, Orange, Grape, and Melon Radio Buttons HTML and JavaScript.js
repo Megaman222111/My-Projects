669a51304ed6041b0103e4b4b572c765628d6d7e
Megaman222111 <74266911+Megaman222111@users.noreply.github.com>
2021-04-09 18:23:04 -0600

    Add files via upload

---
+++ Apple, Orange, Grape, and Melon Radio Buttons HTML and JavaScript.js
@@ -0,0 +1,17 @@
+function checkItem(){
+    if (apple.checked==true){
+        alert("Apple is clicked");
+    }
+    
+    else if (orange.checked==true){
+        alert("Orange is clicked");
+    }
+
+    else if (grape.checked==true){
+        alert("Grape is clicked");
+    }
+
+    else{
+        alert("Melon is clicked");
+    }
+}
